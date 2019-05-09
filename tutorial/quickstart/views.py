90b3e9f04652f17f5ab12d0a87dc97bd91515269
buffard <samuelwebber19@gmail.com>
2019-05-09 16:40:44 -0500

    stopped for the day

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,15 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, PostSerializer, CommentSerializer, CategorySerializer, ContentTypeSerializer
+
+class User(models.Model):
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+
+
+
+
