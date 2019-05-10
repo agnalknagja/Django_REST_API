e5b18852ce52554e133c98315b4457f5e64ecb18
buffard <samuelwebber19@gmail.com>
2019-05-10 09:56:00 -0500

    added models

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -4,12 +4,34 @@ from tutorial.quickstart.serializers import UserSerializer, PostSerializer, Comm
 
 class User(models.Model):
     
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  password = models.CharField(max_length=50)
+  username = models.CharField(max_length=50)
+
+class Post(models.Model):
+  
+  title = models.CharField(max_length=100)
+  content = models.CharField(max_length=100)
+  tags = models.CharField(max_length=25)
+  publish_time = models.Datefield()
+  author_id = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+
+ 
 
 
+
+
+
+
+
+
+class Category(models.Model):
+  name = models.CharField(max_length=25)
+  description = models.CharField(max_length=100)
+
+class ContentType(models.Model):
+  type_name = models.CharField(max_length=25)
 
 
