e2f8bb8bf133ff8561c47d7843eeaa3c0ed083d8
buffard <samuelwebber19@gmail.com>
2019-05-28 13:32:24 -0500

    added serializers, views, urls

---
+++ blog/myapi/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+from myapi import views
+
+router = DefaultRouter()
+router.register('user', views.UserViewSet)
+router.register('post', views.PostViewSet)
+router.register('comment', views.CommentViewSet)
+router.register('category', views.CategoryViewSet)
+router.register('content-type', views.ContentTypeViewSet)
+
+urlpatterns = [
+  path('', include(router.urls))
+]
--- blog/myapi/views.py
+++ blog/myapi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from myapi.models import User, Post, Comment, Category, ContentType
+from myapi.serializers import UserSerializer, PostSerializer, CommentSerializer, CategorySerializer, ContentTypeSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+
+class PostViewSet(viewsets.ModelViewSet):
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+class ContentTypeViewSet(viewsets.ModelViewSet):
+
