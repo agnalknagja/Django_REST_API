4be52269cd490ad2690e93b96ac865a8420a8b7a
buffard <samuelwebber19@gmail.com>
2019-05-28 13:08:32 -0500

    have working models and migrations

---
+++ blog/myapi/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class User(models.Model):  
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  password = models.CharField(max_length=50)
+  username = models.CharField(max_length=50)
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.CharField(max_length=100)
+  tags = models.CharField(max_length=25)
+  publish_time = models.DateField(default=None, null=True)
+  author_id = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+
+class Comment(models.Model):
+  content = models.CharField(max_length=1000)
+  publish_time = models.DateField(default=None, null=True)
+  author_name = models.CharField(max_length=50)
+  author_email = models.CharField(max_length=100)
+  post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+class Category(models.Model):
+  name = models.CharField(max_length=25)
+  description = models.CharField(max_length=100)
+
+class ContentType(models.Model):
+  type_name = models.CharField(max_length=25)
