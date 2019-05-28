299ef6b9926aa1d5a442cf256e37e06de119a13e
buffard <samuelwebber19@gmail.com>
2019-05-28 13:09:54 -0500

    have working models and made migrations

--- tutorial/quickstart/views.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from tutorial.quickstart.serializers import UserSerializer, PostSerializer, CommentSerializer, CategorySerializer, ContentTypeSerializer
-
-
-
-
